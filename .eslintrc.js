2face0c1e7e782f3dce95b055569a7d6bdaed4bb
weili.zeng <weili.zeng@nufront.com>
2019-04-02 11:15:39 +0800

    extend eslint-config-nufront

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,6 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+  env: { node: true },
+  'extends': ['plugin:vue/essential', 'eslint-config-nufront'],
+  parserOptions: { parser: 'babel-eslint' },
+};
